999cc848d95c3c54b852cd2f61f02a5fc27debb5
BrayanRK <98141343+BrayanRK@users.noreply.github.com>
2026-06-17 18:21:14 -0500

    Remove console logs from toimg.js

--- commands/stickers/toimg.js
+++ commands/stickers/toimg.js
@@ -1,15 +1,11 @@
 import { downloadMediaMessage } from "fsociety-Baileys";
 import { stickerAPng } from "./stickerUtils.js";
 
-console.log("✅ TOIMG CARGADO");
-
 export default {
   name: "toimg",
   aliases: ["img"],
 
   run: async (sock, msg, args, jid) => {
-    console.log("🔥 TOIMG EJECUTANDO");
-
     const { reply } = await import("../../utils.js");
 
     try {
@@ -24,18 +20,12 @@ export default {
       const mediaMsg = { message: quoted, key: msg.key };
       const buffer = await downloadMediaMessage(mediaMsg, "buffer", {});
 
-      console.log("📥 Sticker descargado");
-
       const pngBuffer = await stickerAPng(buffer);
-
-      console.log("✅ PNG creado");
 
       await sock.sendMessage(jid, {
         image: pngBuffer,
         caption: "✨ Sticker convertido correctamente"
       }, { quoted: msg });
-
-      console.log("✅ Imagen enviada");
 
     } catch (e) {
       console.error("❌ ERROR TOIMG:", e);
